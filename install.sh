080b2354b06b56f07520f948786fb035634066aa
Stuart Wheater <stuart.wheater@arjuna.com>
2018-03-09 15:03:30 +0000

    Fixed deployment directory name

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ ng version
 
 ng build --dev
 
-if [ -d '../health_new_towns-website' ]; then
-    rm -rf ../health_new_towns-website/*
+if [ -d '../healthy_new_towns-website' ]; then
+    rm -rf ../healthy_new_towns-website/*
 fi
 
 if [ -d 'dist' ]; then
-    mv dist/* ../health_new_towns-website/.
+    mv dist/* ../healthy_new_towns-website/.
 fi
